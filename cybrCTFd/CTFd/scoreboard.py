0e18d2fa3dbcde0f8364bbdb70ac37ae88c983dd
Rob Cobbins <robert.cobbins@cyberark.com>
2019-06-06 13:34:03 -0700

    Changes to support single use flags

---
+++ cybrCTFd/CTFd/scoreboard.py
@@ -0,0 +1,19 @@
+from flask import render_template, Blueprint
+
+from CTFd.utils import config
+from CTFd.utils.decorators.visibility import check_score_visibility
+
+from CTFd.utils.scores import get_standings
+
+scoreboard = Blueprint("scoreboard", __name__)
+
+
+@scoreboard.route("/scoreboard")
+@check_score_visibility
+def listing():
+    standings = get_standings()
+    return render_template(
+        "scoreboard.html",
+        standings=standings,
+        score_frozen=config.is_scoreboard_frozen(),
+    )
---
+++ cybrCTFd/wsgi.py
@@ -0,0 +1,8 @@
+from gevent import monkey
+monkey.patch_all()
+from CTFd import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
